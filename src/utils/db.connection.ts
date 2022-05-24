58db2683dbb770fb063e2d532f555c4aeb07c3db
Filipe Manuel Sampaio Sousa <hadespower@gmail.com>
2022-05-24 18:46:14 +0100

    update

--- src/utils/db.connection.ts
+++ src/utils/db.connection.ts
@@ -12,38 +12,3 @@ export async function connectToDatabase() {
     }
 
 }
-
-
-
-// import { MongoClient } from 'mongodb';
-
-// export async function connectToDatabase(){
-//     /**
-//      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-//      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-//      */
-//     const uri = "mongodb+srv://filipe225:Chocapic1Mongodb@pc-builder-cluster/test?retryWrites=true&w=majority";
- 
-
-//     const client = new MongoClient(uri);
- 
-//     try {
-//         // Connect to the MongoDB cluster
-//         await client.connect();
- 
-//         // Make the appropriate DB calls
-//         await  listDatabases(client);
- 
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         await client.close();
-//     }
-// }
-
-// async function listDatabases(client: any){
-//     const databasesList = await client.db().admin().listDatabases();
- 
-//     console.log("Databases:");
-//     databasesList.databases.forEach( (db: any) => console.log(` - ${db.name}`));
-// };
