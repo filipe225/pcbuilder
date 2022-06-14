825821d10c5abceec3e4b2a090a0517ade4204a8
Filipe Manuel Sampaio Sousa <hadespower@gmail.com>
2022-06-14 22:49:35 +0100

    added new functions

--- src/services/cpu.service.ts
+++ src/services/cpu.service.ts
@@ -1,7 +1,29 @@
 
 import { Request, Response } from "express";
-import { UpdateQuery, FilterQuery } from "mongoose";
-import cpu_model, { cpu_interface } from "../models/cpu.model";
+import cpu_model, { cpu_interface, Architecture, FrequencyUnit, Manufacturer, Socket, TdpUnit, CacheUnit } from '../models/cpu.model';
+import { StoreName } from '../models/store.model';
+
+export async function getAllCpus(req: Request, res: Response) { 
+    try {
+        const cpus = await cpu_model.find();
+        res.render('display_cpus', { cpus });
+        
+    } catch (error: any) {
+        console.error(error);
+        throw new Error(error);
+    }
+}
+
+export function addNewCpu(req: Request, res: Response) {
+    const manufacturer = Object.values(Manufacturer);
+    const socket = Object.values(Socket);
+    const architecture = Object.values(Architecture);
+    const stores = Object.values(StoreName);
+    const frequency_unit = Object.values(FrequencyUnit);
+    const cache_unit = Object.values(CacheUnit);
+    const tdp_unit = Object.values(TdpUnit);
+    res.render('add_cpu', { manufacturer, socket, architecture, stores, frequency_unit, cache_unit, tdp_unit } ); 
+}
 
 export async function createCpu(req: Request, res: Response) {
     try {
@@ -38,12 +60,4 @@ export async function deleteCpu(req: Request, res: Response) {
         console.error(error);
         throw error;
     }
-}
-
-async function getAllCpus() {
-    return cpu_model.find();
-}
-
-async function getFilteredCpus() {
-
 }
