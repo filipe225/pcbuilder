dd83c79284bbcffefce5001048b8be0c4a7ca401
Filipe Manuel Sampaio Sousa <hadespower@gmail.com>
2022-06-14 22:48:54 +0100

    updated routes

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -5,11 +5,6 @@ import 'dotenv/config';
 
 import admin_router from '../routes/admin.routes';
 import { connectToDatabase } from '../utils/db.connection';
-import cpu_model, { Architecture, Manufacturer, Socket } from '../models/cpu.model';
-import { createCpu, updateCpu, deleteCpu } from '../services/cpu.service';
-import { CpuMiddlewareTransform } from '../middleware/cpu.middleware';
-import { StoreName } from '../models/store.model';
-
 
 const app = express();
 
@@ -27,19 +22,8 @@ app.set('view engine', 'pug');
 // ROUTES
 
 // ADMIN ROUTER
+// contains all components routers
 app.use('/', admin_router);
-
-// CPU ROUTES
-app.get('/add_cpu', (req, res) => { 
-    const manufacturer = Object.values(Manufacturer);
-    const socket = Object.values(Socket);
-    const architecture = Object.values(Architecture);
-    const stores = Object.values(StoreName)
-    res.render('add_cpu', { manufacturer, socket, architecture, stores } ); 
-});
-app.post('/add_new_cpu', CpuMiddlewareTransform, createCpu);
-app.put('update_cpu/:id', updateCpu);
-app.delete('delete_cpu/:id', deleteCpu);
 
 
 // ERROR HANDLER
@@ -51,31 +35,3 @@ app.listen(6200, () => {
     console.log("App running at http://localhost:6200")
     connectToDatabase();
 });
-
-// app.get('/', (req: Request, res: Response) => {
-//     res.render('homepage', { 
-//         computers: [
-//             {
-//                 ram: '2x8gb',
-//                 processor: 'ryzen 5 3600',
-//                 graphics_card: '6700xt',
-//                 motherboard: 'aorus elite v2',
-//                 case: 'mastercooler box 500'
-//             },
-//             {
-//                 ram: '2x8gb',
-//                 processor: 'ryzen 5 3600',
-//                 graphics_card: '6700xt',
-//                 motherboard: 'aorus elite v2',
-//                 case: 'mastercooler box 500'
-//             },
-//             {
-//                 ram: '2x8gb',
-//                 processor: 'ryzen 5 3600',
-//                 graphics_card: '6700xt',
-//                 motherboard: 'aorus elite v2',
-//                 case: 'mastercooler box 500'
-//             }
-//         ]
-//      })
-// });
