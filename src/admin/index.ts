4fa023cb9f9757b837a1703a869110fcf3d059f1
Filipe Manuel Sampaio Sousa <hadespower@gmail.com>
2022-06-21 22:54:21 +0100

    updated routes with nested routes

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,6 +1,5 @@
-import express, { Request, Response, NextFunction} from 'express'
-import pug from 'pug'
-import path from 'path'
+import express, { Request, Response, NextFunction} from 'express';
+import path from 'path';
 import 'dotenv/config';
 
 import admin_router from '../routes/admin.routes';
@@ -15,21 +14,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(path.join(__dirname, '/views'))));
 
 // SET ENGINE AS PUG. SET VIEWS DIRECTORY
-app.set('views', path.resolve(path.join(__dirname, './views')));
+app.set('views', [
+            path.resolve(path.join(__dirname, './views')),
+            path.resolve(path.join(__dirname, './views/cpu')),
+            path.resolve(path.join(__dirname, './views/psu')),
+            path.resolve(path.join(__dirname, './views/gpu')),
+            path.resolve(path.join(__dirname, './views/ram')),
+            path.resolve(path.join(__dirname, './views/motherboard')),
+            path.resolve(path.join(__dirname, './views/pccase'))
+        ]);
 app.set('view engine', 'pug');
-
-
-// ROUTES
+// set basedir to be able to have multiple folders inside views
+app.locals.basedir =  path.resolve(path.join(__dirname, './views'));
 
 // ADMIN ROUTER
 // contains all components routers
-app.use('/', admin_router);
+app.use('/admin', admin_router);
+app.get('/', (req, res) => res.redirect('/admin'));
 
 
 // ERROR HANDLER
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.status(400).send(err.message);
-})
+});
 
 app.listen(6200, () => {
     console.log("App running at http://localhost:6200")
