e16bc258cbf203507f7706215e7a2af72fc668c1
Filipe Manuel Sampaio Sousa <hadespower@gmail.com>
2022-06-21 22:54:04 +0100

    added nested routes to admin

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,7 +1,7 @@
 
 import express, { Router, Request, Response, NextFunction } from "express";
 
-import { createCpu, updateCpu, deleteCpu, getAllCpus, addNewCpu } from '../services/cpu.service';
+import { createCpu, updateCpu, deleteCpu, getAllCpus, addNewCpu, getCpuToUpdate, getCpuToDelete } from '../services/cpu.service';
 import { cpuMiddlewareTransform } from '../middleware/cpu.middleware';
 
 /*
@@ -12,23 +12,28 @@ import { cpuMiddlewareTransform } from '../middleware/cpu.middleware';
         /admin/cpu/delete/:cpu_name     (delete)
 */
 
-
-const cpu_router: Router            = express.Router();
-const gpu_router: Router            = express.Router();
-const ram_router: Router            = express.Router();
-const motherboard_router: Router    = express.Router();
-const psu_router: Router            = express.Router();
-const pccase_router: Router         = express.Router();
+const cpu_router: Router            = express.Router({mergeParams: true});
+const gpu_router: Router            = express.Router({mergeParams: true});
+const ram_router: Router            = express.Router({mergeParams: true});
+const motherboard_router: Router    = express.Router({mergeParams: true});
+const psu_router: Router            = express.Router({mergeParams: true});
+const pccase_router: Router         = express.Router({mergeParams: true});
 
 const admin_router: Router          = express.Router();
+
+admin_router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.render('homepage');
+});
 
 
 // CPU ROUTES
 cpu_router.get('/', getAllCpus);
 cpu_router.get('/add_cpu', addNewCpu);
 cpu_router.post('/add_new_cpu', cpuMiddlewareTransform, createCpu);
-cpu_router.put('update_cpu/:id', updateCpu);
-cpu_router.delete('delete_cpu/:id', deleteCpu);
+cpu_router.get('/update_cpu/:id', getCpuToUpdate);
+cpu_router.put('/update_cpu/:id', updateCpu);
+cpu_router.get('/delete_cpu/:id', getCpuToDelete)
+cpu_router.delete('/delete_cpu/:id', deleteCpu);
 
 admin_router.use('/cpu', cpu_router);
 admin_router.use('/gpu', cpu_router);
@@ -37,9 +42,5 @@ admin_router.use('/motherboard', cpu_router);
 admin_router.use('/pccase', cpu_router);
 admin_router.use('/ram', cpu_router);
 admin_router.use('/cooler', cpu_router);
-
-admin_router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.render('homepage');
-});
 
 export default admin_router;
