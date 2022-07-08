7b748cfd73a7c33b722efd3bf8b4730c9868f595
Filipe Manuel Sampaio Sousa <hadespower@gmail.com>
2022-07-08 20:48:33 +0100

    add computer sets

---
+++ src/models/computer_set.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model, Types } from 'mongoose';
+import computer, { computer_interface } from './computer.model';
+
+export interface computer_set_interface {
+    _id?: Types.ObjectId,
+    name: string,
+    computers: Types.DocumentArray<computer_interface>
+    createdAt?: Date,
+    updatedAt?: Date
+}
+
+const computer_set_schema = new Schema<computer_set_interface>( 
+    {
+        name: { type: String, required: true },
+        computers: [computer]
+    },
+    {
+        timestamps: true
+    }
+)
+
+const computer_set = model('computer_set', computer_set_schema);
+export default computer_set;
